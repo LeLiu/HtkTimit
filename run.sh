5409d95c83a33c0f1525a525f16b4ffb015790f1
liule <lele.liu@hotmail.com>
2019-06-14 18:40:58 +0800

    complete step 7

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ log=log
 if [ ! -d $exp ]; then mkdir -p $exp || exit 1; fi
 if [ ! -d $log ]; then mkdir -p $log || exit 1; fi
 << COMMENT
+# Step 2. the Dictionary
 # create transcription file for training set.
 python gen_trans.py $data/train $exp/train_trans || exit 1
 
@@ -25,16 +26,20 @@ HDMan -D -A -T 1 -m -e ded -w $exp/train_wlist -n $exp/monophones -l $log/dlog $
 
 echo sil >> $exp/monophones
 
+# Step 4. Creating the Transcription Files
 # create mlf file.
 python trans2mlf.py $exp/train_trans $exp/train_mlf || exit 1
 
+# create phone level mlf file.
 HLEd -D -A -T 1 -l '*' -d $exp/dict -i $exp/train_phone_mlf ded/mkphone.led $exp/train_mlf || exit 1
 
+# Setp 5. Coding the Data
 if [ ! -d $exp/feat ]; then mkdir -p $exp/feat || exit 1; fi
 python gen_feat_scp.py $data/train $exp/feat $exp/train_feat_scp || exit 1
 
 HCopy -D -A -T 1 -C config/hcopy.conf -S exp/train_feat_scp
 
+# Step 6 Create Flat Start Monophones
 awk '{print $2}' $exp/train_feat_scp > $exp/train_scp
 HCompV -D -A -T 1 -C config/hcompv.conf -f 0.01 -m -S $exp/train_scp -M hmm/0 hmm/proto
 
@@ -47,6 +52,7 @@ HERest -D -A -T 1 -C config/hcompv.conf -I exp/train_phone_mlf -t 250 150 1000 -
 HERest -D -A -T 1 -C config/hcompv.conf -I exp/train_phone_mlf -t 250 150 1000 -S exp/train_scp -H hmm/2/macros -H hmm/2/hmmdefs  -M hmm/3 exp/monophones
 
 COMMENT
+
 # Step 7. Fixing the Silence Models
 mkdir hmm/4
 python sil2sp.py hmm/3/hmmdefs hmm/4/sp
@@ -58,5 +64,11 @@ echo sp >> $exp/monophones_sp
 mkdir hmm/5
 HHEd -D -A -T 1 -H hmm/4/macros -H hmm/4/hmmdefs -M hmm/5 ded/sil.hed $exp/monophones_sp
 
+mkdir hmm/6
+mkdir hmm/7
+HERest -D -A -T 1 -C config/hcompv.conf -I exp/train_phone_mlf -t 250 150 1000 -S exp/train_scp -H hmm/5/macros -H hmm/5/hmmdefs  -M hmm/6 exp/monophones_sp
+HERest -D -A -T 1 -C config/hcompv.conf -I exp/train_phone_mlf -t 250 150 1000 -S exp/train_scp -H hmm/6/macros -H hmm/6/hmmdefs  -M hmm/7 exp/monophones_sp
 
+# Step 8. Realing the Training Data
 
+#HVite -l '*' -o SWT -b silence -C config -a -H hmm/7/macros -H hmm7/hmmdefs -i aligned.mlf -m -t 250 150 1000 -y lab -I words.mlf -S train.scp dict exp/monophones_sp
